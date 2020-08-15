5ddf076aa7b0567617194529f341cc25b5a4c3fb
fulln <245193247@qq.com>
2020-08-15 19:46:12 +0800

    :star: 添加服务器登录的脚本

---
+++ ssh_script/locate.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+hostip="" 
+username=""
+passwd=""
+location=""
+
+## 计数器
+index=1
+## 未输入参数
+if [ -z "$1" ];
+then                           
+   echo "Usage: will be used default params"
+   hostip="${hostip}24"
+else
+   echo "listing args with \$*:"
+   for arg in  $*
+   do 
+	   echo arg: $index = $arg
+	   if [ $index  == 1 ];
+	   then		   
+	   	hostip="$hostip$1" 
+	   elif [ $index == 2 ];
+	   then
+		username=$arg
+	   elif [ $index == 3 ];
+	   then
+	   	passwd=$arg
+	   else [ $index == 4 ];
+   		location=$arg	
+     	   fi
+           let index += 1	   
+   done    
+   echo   
+      	   
+fi
+
+echo  $hostip $username $passwd $location
+
+expect login.exp $hostip $username $passwd $location
+
